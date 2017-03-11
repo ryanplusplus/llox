75278ffac74e622908b8dd1d4e9c7932bbfac255
Ryan Hartlage <ryanplusplus@gmail.com>
2017-03-11 10:41:30 -0500

    Extracted parenthesize helper

--- src/print_visitor.lua
+++ src/print_visitor.lua
@@ -1,13 +1,17 @@
+local function parenthesize(...)
+  return '(' .. table.concat(table.pack(...), ' ') .. ')'
+end
+
 local function visit(node)
   return ({
     unary = function()
-      return '(' .. node.operator .. ' ' .. visit(node.left) .. ')'
+      return parenthesize(node.operator, visit(node.left))
     end,
     binary = function()
-      return '(' .. node.operator .. ' ' .. visit(node.left) .. ' ' .. visit(node.right) .. ')'
+      return parenthesize(node.operator, visit(node.left), visit(node.right))
     end,
     grouping = function()
-      return '(group ' .. visit(node.expression) .. ')'
+      return parenthesize('group', visit(node.expression))
     end,
     literal = function()
       return tostring(node.value)
